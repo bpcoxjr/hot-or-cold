fdb620fc5a538b1fff89270d4ce66a9050409e0a
Bruce P Cox Jr <bpcoxjr@yahoo.com>
2015-09-03 22:34:18 -0400

    Updated local branch

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 
 $(document).ready(function(){
-//First, generate a random number between 1 and 100
+	//First, generate a random number between 1 and 100
 
-var randomNum = Math.ceil(Math.random() * 100);
-console.log(randomNum);
+	var randomNum = Math.ceil(Math.random() * 100);
+	console.log('randomNum: ' + randomNum);
 	
 
 	/*--- Display information modal box ---*/
@@ -17,83 +17,84 @@ console.log(randomNum);
   		$(".overlay").fadeOut(1000);
   	});
 
-//New game begins by taking a guess from the user and storing in the variable "guess", which is then parsed(converted from string to integer)
+	//New game begins by taking a guess from the user and storing in the variable "guess", 
+	//which is then parsed(converted from string to integer)
 
-var guess;
-var counter = 0;
-
-
-
-$('#guessButton').click(function(){
-	//alert("Here");
-	guess = $('#userGuess').val();
-	guess = parseInt(guess);
-	counter++;
-	console.log(counter);
-	$('span').text(counter);
-
-	//checks that user input is greater than 1 and less than 100, and if so, runs function hotorcold
-if (guess % 1 === 0 && guess >= 1 && guess <=100){
-	hotorcold(guess);
-}
-//if user inputs number not between 1 and 100 they are alerted to try again
-else {
-	alert("The number you enter must be between 1 and 100! Try again!");
-		$('.user-input').val ("")
-}
-});
+	var guess;
+	var counter = 0;
 
 
-	var compareNumNew;
-	var compareNumOld = 99999;
+
+	$('#guessButton').click(function(){
+		guess = $('#userGuess').val();
+		guess = parseInt(guess);
+		counter++;
+		console.log(counter);
+		$('span').text(counter);
+
+
+		//checks that user input is greater than 1 and less than 100, and if so, runs function hotorcold
+	if (guess % 1 === 0 && guess >= 1 && guess <=100){
+		hotorcold(guess);
+	}
+	//if user inputs number not between 1 and 100 they are alerted to try again
+	else {
+		alert("The number you enter must be between 1 and 100! Try again!");
+			$('.user-input').val ("")
+	}
+	});
+
+	var lastGuess;
 
 	function hotorcold(guess) {
 		
-		if (guess <= 100) {
+		if (guess > 100) return;
 			$('#guessList').append("<li>" + guess + "</li>");
 			console.log(guess);
-			compareNumOld = compareNumNew;
-			compareNumNew = guess;
 
 			var compareNum = Math.abs(guess - randomNum);
 			console.log(compareNum);
+
 			//Cold & warm logic	
 			if (compareNum >= 50) {
-				$('#feedback').html("<h2>" + "Ice cold!" + "</h2>");
+				$('#feedback h2').text("Ice cold!");
 			}
 
-			else if (compareNum >= 30 && compareNum < 50) {
-				$('#feedback').html("<h2>" + "Pretty cold!" + "</h2>");
+			else if (compareNum >= 30) {
+				$('#feedback h2').text("Pretty cold!");
 			}
 
-			else if (compareNum >= 20 && compareNum <30) {
-				$('#feedback').html("<h2>" + "Warm-ish!" + "</h2>");
+			else if (compareNum >= 20) {
+				$('#feedback h2').text("Warm-ish!");
 			}
 
-			else if (compareNum >= 10 && compareNum < 20) {
-				$('#feedback').html("<j2>" + "Warm!" + "</h2>");
+			else if (compareNum >= 10) {
+				$('#feedback h2').text("Warm!");
 			}
 
-			else if (compareNum > 1 && compareNum <= 10) {
-				$('#feedback').html("<h2>" + "Hot!" + "</h2>");
+			else if (compareNum > 1) {
+				$('#feedback h2').text("Hot!");
 			}
 			else if (guess == randomNum) {
-				$('#feedback').html("<h2>" + "You got it!" + "</h2>");
+				$('#feedback h2').text("You got it!");
+				$('#feedback h3').text('');
 			}
+
 			//Colder & warmer logic
-			if (compareNumOld !== 99999) {
-			
-			//alert(compareNumOld);
-			//alert(compareNumNew - guess);
-				if ((compareNumNew - guess) > (compareNumOld - guess)) {
-					$('#feedback').append("<h3>" + "You're getting warmer!" + "</h3>");
-				}
-				else {
-					$('#feedback').append("<h3>" + "You're getting colder!" + "</h3>");
-				}
+		if(lastGuess !== undefined) { //second guess onward
+			if ((lastGuess - guess) < 0) {
+				$('#feedback h3').text("But you're getting warmer!");
 			}
-		}
-		
+			else { 
+				$('#feedback h3').text("But you're getting colder!");
+			}
+		} else { //first guess
+				$('#feedback h3').text("Not bad for a first guess!");
+			}
+
+			$('#feedback h3').show();
+
+		lastGuess = guess;
 	};
 
 	$('.new').click(function(){
